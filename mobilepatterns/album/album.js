f546fa4fa4f804b450de4a05d9e6f010fd8abb20
Morgan Cheng <morgan.chengmo@gmail.com>
2011-11-25 17:28:14 +0800

    rotate is done

--- mobilepatterns/album/album.js
+++ mobilepatterns/album/album.js
@@ -2,17 +2,26 @@ YUI.add('album', function(Y) {
 
 var FLICK_MIN_DISTANCE = 15;
 
+//
+// Y.Album represents card-fold
+//
 Y.Album = Y.Base.create('album', Y.Widget, [],
 {
     initializer: function() {
         this.cb = this.get('contentBox');
+        this.cardFolds = this.cb.all('.card-fold');
     },
 
     destructor: function() {
     },
 
     renderUI: function() {
-        this.fold();
+        this.cardWidth = parseInt(this.cardFolds.item(0).getComputedStyle('width'));
+        this.cardHeight = parseInt(this.cardFolds.item(0).getComputedStyle('height'));
+
+        this.cb.setStyle('height', this.cardHeight);
+
+        this.collapse();
     },
 
     bindUI: function() {
@@ -21,29 +30,110 @@ Y.Album = Y.Base.create('album', Y.Widget, [],
                 endTarget = e.target,
                 distance = e.flick.distance;
 
-            if (this.folded && distance > 0) {
-                this.unfold();
+            if (this._collpased && distance > 0) {
+                this.expand();
             }
-            if (!this.folded && distance < 0) {
-                this.fold();
+            if (!this._collpased && distance < 0) {
+                this.collapse();
             }
         }, {
             minDistance: FLICK_MIN_DISTANCE,
             axis: 'x',
+            //preventDefault: true, // Wired, if preventDefault, 'click' delegate below would not trigger in iPad
             minVelocity: 0,
-            preventDefault: true
+        }, this);
+
+        this.cb.delegate('click', function(e) {
+            if (!this._collpased) {
+                this.unfold(e.currentTarget);
+            }
+        }, '.card-fold', this);
+    },
+
+    unfold: function(cardFold) {
+        if (cardFold.getData('folding')) {
+            return;
+        }
+        cardFold.setData('folding', true);
+
+        cardFold.addClass('unfolded');
+
+        var cards = cardFold.all('.single-card');
+
+        this.rotate(cards);
+
+        cardFold.setData('folding', false);
+    },
+
+    rotate: function(cards) {
+        var coverCard = cards.item(0),
+            card1 = cards.item(1),
+            card2 = cards.item(2),
+            card3 = cards.item(3),
+            duration = this.get('unfoldDuration'),
+            that = this;
+
+        if (!card1) {
+            return;
+        }
+
+        coverCard.setStyles({
+            '-webkit-transform-origin' : 'right center',
+            'z-index': 4
+        });
+
+        card1.removeClass('hidden');
+
+        coverCard.transition({
+            duration: duration,
+            easing: 'ease-in-out',
+            'transform': 'rotateY(180deg)',
+        }, function() {
+
+            // continus to flip
+            card2.setStyles({
+                left: that.cardWidth
+            }).removeClass('hidden');
+
+            coverCard.setStyles({
+                '-webkit-transform-origin' : 'left center',
+                '-webkit-transform' : 'rotateY(180deg)',
+                'left' : 2 * that.cardWidth,
+                'z-index': 4
+            }).transition({
+                easing: 'ease-in-out',
+                'transform' : 'rotateY(360deg)',
+                duration: duration
+            });
+
+            card3.setStyles({
+                '-webkit-transform-origin' : 'left center',
+                '-webkit-transform' : 'rotateY(180deg)',
+                'left' : 2 * that.cardWidth,
+                'z-index': 4
+            }).removeClass('hidden').transition({
+                easing: 'ease-in-out',
+                'transform' : 'rotateY(360deg)',
+                duration: duration
+            }, function() {
+                 
+            });
+
+        });
+
+        cards.each(function(card, idx) {
         }, this);
     },
 
-    unfold: function() {
-        this.fold(true);
+    expand: function() {
+        this.collapse(true);
     },
 
-    fold: function(unfold) {
+    collapse: function(expand) {
         var contentBox = this.get('contentBox'),
             offsetX = 0;
 
-        var cards = contentBox.all('.album-card');
+        var cards = contentBox.all('.card-fold');
         nodeCount = cards.size();
 
         cards.each(function(node, idx, list) {
@@ -54,16 +144,19 @@ Y.Album = Y.Base.create('album', Y.Widget, [],
                 'z-index': nodeCount - idx,
             });
 
-            offsetX += unfold ? this.get('unfoldedDistance') : this.get('foldedDistance');
+            offsetX += expand ? this.get('unfoldedDistance') : this.get('foldedDistance');
         }, this);
 
-        this.folded = !unfold;
+        this._collpased = !expand;
     }
 
 },
 {
     NAME: 'album',
     ATTRS: {
+        unfoldDuration: {
+            value: 0.5
+        },
         unfoldedDistance: {
             value: 160
         },
